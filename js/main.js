b76cb18fc1897d9132bfc64c7cc3635f7de0b5a2
yj.j <bab0503@naver.com>
2023-03-08 18:02:01 +0900

    전체 레이아웃 수정

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// 메뉴 버튼 클릭 이벤트
-
-function open_menu(){
-
-    document.querySelector(".wrap").style.left="0px";
-    document.querySelector(".btn_menu").style.left="330px";
-    document.querySelector(".icon_open").style.display="none";
-    document.querySelector(".icon_close").style.display="block";    
-};
-
-function close_menu(){
-
-    document.querySelector(".wrap").style.left="-300px";
-    document.querySelector(".btn_menu").style.left="30px";
-    document.querySelector(".icon_open").style.display="block";
-    document.querySelector(".icon_close").style.display="none";   
-};
-
-
-
 // 비쥬얼메인 스와이퍼
 var swiper = new Swiper(".visual", {
     slidesPerView: 1,
